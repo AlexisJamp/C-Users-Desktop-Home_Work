5f993ea0a28249c9361815b9d4ca4bcb94bd3133
AlexisJamp <154271476+AlexisJamp@users.noreply.github.com>
2024-01-22 11:14:21 +0200

    Add files via upload

--- Manzia_Oleksii_Front-end_Pro_DZ_12/main.js
+++ Manzia_Oleksii_Front-end_Pro_DZ_12/main.js
@@ -1,49 +1,11 @@
-// Дз 12
+// ДЗ 12
 
-// 1
 let arr = [];
+arr = prompt('Enter an array').split(' ')
+arr.length = prompt('Enter a number that will be array length: ');
 
-for (let i=0; i <= 30; i = i+0.5){
-    arr.push(i)
-}
+
+
 console.log(arr.join(' '))
-
-// 2 
-let dollar = 27;
-let currency = [];
-for (let i = 10; i <= 100; i = i+10){
-    currency.push(i*dollar)
-}
-console.log(currency.join('-'))
-
-// 3
-let num = prompt('Enter a number: ')
-let squareList = [];
-for (let i = 1; i <=100; i++){
-    if (i**2 < num){
-        squareList.push(i)
-    }
-}
-console.log(squareList.join('-'));
-
-// 4
-let hallNumber = prompt('Enter a number: ')
-let hallNumberList = [];
-for (let i = hallNumber; i >= 0; i--){
-    if (i >= 1 && hallNumber%i == 0){
-        hallNumberList.push(i);
-    }
-}
-if (hallNumberList.length <= 2){
-    console.log('Это число простое.');
-} else {
-    console.log('Это число не простое');
-}
-
-// 5
-let squareFm3 = prompt('Enter a number: ')
-if (Math.sqrt(squareFm3) == 3){
-    console.log('Это число степень числа.')
-} else {
-    console.log('Это число не является степенью числа 3.')
-}
+console.log(arr.splice(2,3))
+console.log(arr.sort())

78e00bacafd27cb8117e745c16860d27bc147049
AlexisJamp <154271476+AlexisJamp@users.noreply.github.com>
2024-01-30 22:19:23 +0200

    Add files via upload

--- Manzia_Oleksii_Front-end_Pro_DZ_17/main.js
+++ Manzia_Oleksii_Front-end_Pro_DZ_17/main.js
@@ -3,7 +3,7 @@
 
 // 1. Вычисление среднего арифметического чисел из массива
 
-/* function Avarage(){
+function Avarage(){
     let myArr = prompt('Enter your array: ').split('');
     let avarage = 0;
     counter = 0;
@@ -16,9 +16,12 @@
     return avarage/counter;
 }
 
-console.log(Avarage()); */
+console.log(Avarage());
 
-// 2. Функция doMath +, -, *, /, %, ^ (ступінь ).
+
+
+// 2. Функция doMath.
+
 let x = prompt('Enter first number: ');
 let y = prompt('Enter second number: ');
 let znak = prompt('Enter a math symbol: ');
@@ -27,11 +30,57 @@ function doMath(x, y, znak){
     let result = 0;
     switch (znak){
         case '+':
-            console.log( result = Number(x) + Number(y));
-            break
-        
+            return result = Number(x) + Number(y);
+        case '-':
+            return result = Number(x) - Number(y);
+        case '*':
+            return result = Number(x) * Number(y);
+        case '/':
+            return result = Number(x) / Number(y);
+        case '%':
+            return result = Number(x) % Number(y);
+        case '**':
+            return result = Number(x) ** Number(y);
     }
 }
 
-doMath()
-// 3. 
+console.log(doMath(x,y,znak))
+
+
+// 3. Заполнение массивов
+
+let rows = prompt("Enter a number of strokes:");
+let column = prompt("Enter a number of columns:");
+
+function fillArray(rows, column) {
+    let array = [];
+    for (let i = 0; i < rows; i++) {
+        array[i] = [];
+        for (let j = 0; j < column; j++) {
+            let enteredData = prompt(`Введите элемент для строки ${i + 1}, столбца ${j + 1}:`);
+            array[i][j] = enteredData;
+        }
+    }
+    return array;
+}
+
+console.log(fillArray(rows, column));
+
+
+//4. Удаление ненуждных символов
+
+let inputString = prompt('Enter a string: ');
+let toRemove = prompt('Enter a caracters to remove throu coma').split(', ');
+
+function removeCharactersFromString(inputString, toRemove) {
+    let charSet = new Set(toRemove);
+
+    let resultArray = inputString.split('').filter(char => !charSet.has(char));
+
+    return resultArray.join('');
+}
+
+let result = removeCharactersFromString(inputString, toRemove);
+
+console.log(result);
+

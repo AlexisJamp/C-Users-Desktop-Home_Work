c11eba431e7421c0f3400f6c5390d698d6ef188e
AlexisJamp <154271476+AlexisJamp@users.noreply.github.com>
2024-01-31 18:12:04 +0200

    Add files via upload

--- Manzia_Oleksii_Front-end_Pro_DZ_18/main.js
+++ Manzia_Oleksii_Front-end_Pro_DZ_18/main.js
@@ -3,12 +3,12 @@ let num = prompt('Enter number: ');
 let degree = prompt('Enter degree: ');
 
 function pow(num, degree){
-    if (num == 1){
-        return num;
-    } else {
-        return num ** degree
+    if (degree === 0){
+        return 1;
     }
+    return num * pow(num, degree-1)
+    
 }
 
-
 console.log(pow(num, degree));
+
